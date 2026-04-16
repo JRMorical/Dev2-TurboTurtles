d55e88685ce41ec4f5dc2b06031b77626bb7fec7
viKC vinegar <FutureHendriKCs@gmail.com>
2026-04-16 07:43:56 -0500

    enemy AI, spawner system, tags, animations

--- TurboTurtles/Assets/Scripts/AttackZone.cs
+++ TurboTurtles/Assets/Scripts/AttackZone.cs
@@ -2,23 +2,8 @@ using UnityEngine;
 
 public class AttackZone : MonoBehaviour
 {
-    public enemyAI enemy;
     void Start()
     {
-        enemy = GetComponentInParent<enemyAI>();
-    }
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.CompareTag("Player"))
-        {
-            enemy.inMeleeAttackRange = true;
-        }
-    }
-    private void OnTriggerExit(Collider other)
-    {
-        if(other.CompareTag("Player"))
-        {
-            enemy.inMeleeAttackRange = false;
-        }
+      
     }
 }
--- TurboTurtles/Assets/Scripts/enemyAI.cs
+++ TurboTurtles/Assets/Scripts/enemyAI.cs
@@ -1,24 +1,31 @@
 using UnityEngine;
 using UnityEngine.AI;
 using System.Collections;
+using Unity.VisualScripting;
 
 public class enemyAI : MonoBehaviour, IDamage
 {
-    enum MovementState
-    {
-        CHASING,
-        ATTACKING
-    }
     enum AttackType
     {
         IDLE,
         MELEE,
         RANGED
     }
-  
+    public enum EnemyRole
+    {
+        MELEE_ONLY,
+        RANGED_ONLY,
+        SPAWNER
+    }
+
+    [Header("Role")]
+    [SerializeField] EnemyRole role;
+
     [Header("Enemy Stats")]
     [SerializeField] int HP;
     [SerializeField] int enemyRotateSpeed = 5;
+    [Range(1, 3)][SerializeField] float meleeRange;
+    [Range(13, 25)][SerializeField] float rangedRange;
     [SerializeField] Renderer model;
     [SerializeField] NavMeshAgent agent;
 
@@ -26,59 +33,108 @@ public class enemyAI : MonoBehaviour, IDamage
     [SerializeField] GameObject bullet;
     [SerializeField] float shootRate = 1f;
     [SerializeField] Transform shootPos;
-
     float shootTimer;
 
-    [SerializeField] enemyAnimator anim;
+    enemyAnimator anim;
     Color colorOrig;
 
-    MovementState currentState;
-    AttackType currentType;
+    [Header("Spawner")]
+    [SerializeField] GameObject meleePrefab;
+    [SerializeField] GameObject rangedPrefab;
+    [SerializeField] float spawnCooldown = 5f;
+    [SerializeField] float spawnRadius = 8f;
+    [SerializeField] int maxSpawned = 3;
+    int currentSpawned;
+    enemyAI spawner;
+
     [Header("Attacking")]
-    public bool inRangedAttackRange;
-    public bool inMeleeAttackRange;
-    public bool canAttack;
+    AttackType currentType;
+    float spawnTimer;
+    bool isTravesingOffMeshLink;
     float attackCooldown;
 
+    [Header("Player Position")]
     Vector3 playerDir;
+    Vector3 player;
 
     void Start()
     {
         colorOrig = model.material.color;
         gamemanager.instance.updateGameGoal(1);
         anim = GetComponent<enemyAnimator>();
+        SetEnemyRole();
     }
     void Update()
     {
+        shootTimer += Time.deltaTime;
+        attackCooldown -= Time.deltaTime;
+        spawnTimer += Time.deltaTime;
         EnemyTypeActions();
+    }
+    void SetEnemyRole()
+    {
+        if (CompareTag("Melee"))
+            role = EnemyRole.MELEE_ONLY;
+
+        else if (CompareTag("Ranged"))
+            role = EnemyRole.RANGED_ONLY;
+
+        else if (CompareTag("Spawner"))
+            role = EnemyRole.SPAWNER;
     }
     void EnemyTypeActions()
     {
-        agent.SetDestination(gamemanager.instance.player.transform.position);
         playerDir = gamemanager.instance.player.transform.position - transform.position;
-        attackCooldown -= Time.deltaTime;
-        if (inMeleeAttackRange && attackCooldown <= 0)
+        player = gamemanager.instance.player.transform.position;
+
+        float dist = Vector3.Distance(player, transform.position);
+
+        switch (role)
         {
-            currentState = MovementState.ATTACKING;
-            currentType = AttackType.MELEE;
-            Melee();
+            case EnemyRole.MELEE_ONLY:
+                currentType = dist <= meleeRange ? AttackType.MELEE : AttackType.IDLE;
+                break;
+
+            case EnemyRole.RANGED_ONLY:
+                currentType = dist <= rangedRange ? AttackType.RANGED : AttackType.IDLE;            
+                break;
+
+            case EnemyRole.SPAWNER:
+                TrySpawn();
+                break;
         }
-        else if (inRangedAttackRange)
+        ExecuteStateActions();
+    }
+    void ExecuteStateActions()
+    {
+        if (isTravesingOffMeshLink) return;
+        switch(currentType)
         {
-            currentState = MovementState.ATTACKING;
-            currentType = AttackType.RANGED;
-            Attack();
-        }
-        else
-        {
-            currentState = MovementState.CHASING;
-            currentType = AttackType.IDLE;
-            Chase();
+            case AttackType.MELEE:
+                if(attackCooldown <= 0)
+                {
+                    Melee();
+                    attackCooldown = 0.5f;
+                }
+                break;
+
+            case AttackType.RANGED:
+                Attack();
+                break;
+
+            case AttackType.IDLE:
+                Chase();
+                break;
+
+            default:
+                break;
         }
     }
     void Chase()
     {
-        agent.SetDestination(gamemanager.instance.player.transform.position);
+        agent.isStopped = false;
+        agent.SetDestination(player);
+        rotateToPlayer();
         if(agent.isOnOffMeshLink)
         {
             StartCoroutine(LinkJump());
@@ -86,11 +142,10 @@ public class enemyAI : MonoBehaviour, IDamage
     }
     void Attack()
     {
-        agent.SetDestination(transform.position);
-
+        agent.isStopped = true;
+        shootPos.transform.LookAt(new Vector3(player.x, player.y, player.z));
         rotateToPlayer();
-
-        shootTimer += Time.deltaTime;
+        anim.PlayProjectile();
 
         if (shootTimer >= shootRate)
         {
@@ -99,13 +154,12 @@ public class enemyAI : MonoBehaviour, IDamage
         }
     }
    void Melee()
-    { 
-        if (anim == null || agent == null) return;
-  
-        attackCooldown = .5f;
+    {
+        agent.isStopped = true;
+        transform.LookAt(new Vector3(player.x, player.y, player.z));
+
         rotateToPlayer();
         anim.PlayAttack();
-        agent.SetDestination(transform.position);
     }
     void Shoot()
     {
@@ -113,6 +167,36 @@ public class enemyAI : MonoBehaviour, IDamage
         {
             Instantiate(bullet, shootPos.position, shootPos.rotation);
         }
+    }
+    void TrySpawn()
+    {
+        if (spawnTimer < spawnCooldown) return;
+        if (currentSpawned >= maxSpawned) return;
+
+        spawnTimer = 0;
+        currentSpawned++;
+
+        GameObject prefabToSpawn;
+
+        int roll = Random.Range(0, 2);
+
+        if (roll == 0)
+            prefabToSpawn = meleePrefab;
+        else
+            prefabToSpawn = rangedPrefab;
+
+        Vector3 offset = Random.insideUnitSphere * spawnRadius;
+        offset.y = 0;
+
+        Vector3 pos = transform.position + offset + Vector3.right * Random.Range(-2f, 2f); ;
+
+        GameObject enemy = Instantiate(prefabToSpawn, pos, transform.rotation);
+        enemyAI ai = enemy.GetComponent<enemyAI>();
+        ai.SetSpawner(this);
+    }
+    public void SetSpawner(enemyAI s)
+    {
+        spawner = s;
     }
     void rotateToPlayer()
     {
@@ -122,9 +206,11 @@ public class enemyAI : MonoBehaviour, IDamage
     IEnumerator LinkJump()
     {
         OffMeshLinkData data = agent.currentOffMeshLinkData;
-
+        
         agent.isStopped = true;
         agent.updateRotation = false;
+        isTravesingOffMeshLink = true;
+
         Vector3 start = transform.position;
         Vector3 end = data.endPos + Vector3.up * agent.baseOffset;
 
@@ -139,16 +225,26 @@ public class enemyAI : MonoBehaviour, IDamage
             yield return null;
         }
         transform.position = end;
-        agent.CompleteOffMeshLink();
+
+        agent.Warp(end);
         agent.isStopped = false;
         agent.updateRotation = true;
+        agent.CompleteOffMeshLink();
+
+        isTravesingOffMeshLink = false;
     }
     public void takeDamage(int amount)
     {
         HP -= amount;
-
+  
         if (HP <= 0)
         {
+            //if (role == EnemyRole.SPAWNER)
+            //{
+            //    currentSpawned--;
+            //}             
+            if (spawner != null)
+                spawner.currentSpawned--;
             gamemanager.instance.updateGameGoal(-1);
             Destroy(gameObject);
         }
