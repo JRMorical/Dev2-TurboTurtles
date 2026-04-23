6847d7a2758c63f71e4b09970c73bd8db1a2b523
viKC vinegar <FutureHendriKCs@gmail.com>
2026-04-23 14:38:50 -0500

    Removed unused scripts i made early on

--- TurboTurtles/Assets/Scripts/AttackZone.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class AttackZone : MonoBehaviour
-{
-    void Start()
-    {
-      
-    }
-}
--- TurboTurtles/Assets/Scripts/RangedZone.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class RangedZone : MonoBehaviour
-{
-    void Start()
-    {
-
-    }
-}
